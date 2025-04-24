6fe3b47cc37abf41a886cbabee7b8b60b094a101
etikhonov <evgeniy.tikhonov@gmail.com>
2025-04-24 10:06:43 +0300

    isMock

--- IONGeolocationLib/IONGLOCPositionModel.swift
+++ IONGeolocationLib/IONGLOCPositionModel.swift
@@ -9,8 +9,9 @@ public struct IONGLOCPositionModel: Equatable {
     private(set) public var speed: Double
     private(set) public var timestamp: Double
     private(set) public var verticalAccuracy: Double
+    private(set) public var isMock: Bool
 
-    private init(altitude: Double, course: Double, horizontalAccuracy: Double, latitude: Double, longitude: Double, speed: Double, timestamp: Double, verticalAccuracy: Double) {
+    private init(altitude: Double, course: Double, horizontalAccuracy: Double, latitude: Double, longitude: Double, speed: Double, timestamp: Double, verticalAccuracy: Double, isMock: Bool) {
         self.altitude = altitude
         self.course = course
         self.horizontalAccuracy = horizontalAccuracy
@@ -19,10 +20,31 @@ public struct IONGLOCPositionModel: Equatable {
         self.speed = speed
         self.timestamp = timestamp
         self.verticalAccuracy = verticalAccuracy
+        self.isMock = isMock
     }
 }
 
+
+
 public extension IONGLOCPositionModel {
+
+    static func getIsMock(from location: CLLocation) -> Bool {
+        var isMock = false
+        if #available(iOS 15.0, *) {
+            let isLocationSimulated = location.sourceInformation?.isSimulatedBySoftware ?? false
+            let isProducedByAccess = location.sourceInformation?.isProducedByAccessory ?? false
+                
+            let info = CLLocationSourceInformation(softwareSimulationState: isLocationSimulated, andExternalAccessoryState: isProducedByAccess)
+                
+            if info.isSimulatedBySoftware == true || info.isProducedByAccessory == true{
+                isMock = true
+            } else {
+                isMock = false
+            }
+        }
+        return isMock
+    }
+    
     static func create(from location: CLLocation) -> IONGLOCPositionModel {
         .init(
             altitude: location.altitude,
@@ -32,7 +54,8 @@ public extension IONGLOCPositionModel {
             longitude: location.coordinate.longitude,
             speed: location.speed,
             timestamp: location.timestamp.millisecondsSinceUnixEpoch,
-            verticalAccuracy: location.verticalAccuracy
+            verticalAccuracy: location.verticalAccuracy,
+            isMock: getIsMock(from: location)
         )
     }
 }
